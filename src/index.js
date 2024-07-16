0a4a9d13a14f0ac02f90d4bdce63dc87ef592360
adriane-albani <adriane.albani2002@gmail.com>
2024-07-15 21:49:39 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import ServerlessHttp from 'serverless-http';
+
+const app = express();
+
+app.get("/.netlify/functions/init", (req, res) => {
+    return res.json({messagens: 'Hello world'});
+})
+
+const handler = ServerlessHttp(app);
+
+module.exports.handler = async(event, context) => {
+    const result = await handler(event, context);
+    return result;
+}
