5c15474270ca640eb80b3d7e8bbd231fd7ed9ce7
adriane-albani <adriane.albani2002@gmail.com>
2024-07-23 22:02:18 -0300

    wip: testes configuração 4

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,11 @@ import ServerlessHttp from 'serverless-http';
 
 const app = express();
 
-app.get("/index", (req, res) => {
+app.get("/.netlify/functions/index", (req, res) => {
     return res.json({messagens: 'Hello world'});
 })
 
 const handler = ServerlessHttp(app);
-
 module.exports.handler = async(event, context) => {
     const result = await handler(event, context);
     return result;
